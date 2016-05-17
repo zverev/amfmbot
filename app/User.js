a6f4e0f9877e63010de1d5361007b1e4f066123f
zverev <alexander.zverev@gmail.com>
2016-05-17 03:00:27 +0300

    User.ensureUser static method

--- app/User.js
+++ app/User.js
@@ -18,6 +18,10 @@ let schema = new mongoose.Schema({
     username: {
         type: String
     },
+    isBot: {
+        type: Boolean,
+        default: false
+    },
     lastStations: [String]
 });
 
@@ -30,6 +34,38 @@ schema.methods.pushStation = function(station) {
             }
         }
     }
+}
+
+schema.statics.ensureUser = function(tgUser, isBot) {
+    return new Promise((resolve, reject) => {
+        this.findOne({
+            id: tgUser.id
+        }, (err, result) => {
+            if (err) {
+                return reject(err);
+            }
+
+            if (result) {
+                resolve(result);
+            } else {
+                let user = new this({
+                    id: tgUser['id'],
+                    username: tgUser['username'],
+                    firstName: tgUser['first_name'],
+                    lastName: tgUser['last_name'],
+                    isBot: isBot
+                });
+
+                user.save((err, result) => {
+                    if (!err || err.code === 11000) {
+                        resolve(user);
+                    } else {
+                        reject(new Error('error creating user'));
+                    }
+                });
+            }
+        });
+    });
 }
 
 module.exports = mongoose.model('User', schema);
--- app/index.js
+++ app/index.js
@@ -1,24 +1,39 @@
 'use strict';
 
 const initTelegramCommands = require('./initTelegramCommands.js');
-const ensureBotUser = require('./ensureBotUser.js');
 const mongoose = require('./mongoose.js');
 const config = require('./config.js');
+const User = require('./User.js');
 
 let tg = require('telegram-node-bot')(config.telegram.token);
 
+function getMe(tg) {
+    return new Promise((resolve, reject) => {
+        tg.call('getMe', {}, (resp) => {
+            if (!resp.ok) {
+                reject(new Error('cannot get telegram bot user info'));
+            } else {
+                resolve(resp.result);
+            }
+        })
+    });
+}
+
 Promise.resolve(true)
-    .then(function () {
-        return ensureBotUser(tg);
+    .then(function() {
+        return getMe(tg);
+    })
+    .then(function(tgUser) {
+        return User.ensureUser(tgUser, true);
     })
     .then(function () {
         initTelegramCommands(tg);
     })
-    .then(function () {
+    .then(function() {
         console.log('ready!');
-    }, function (err) {
+    }, function(err) {
         console.error(err.message);
-        mongoose.disconnect(function () {
+        mongoose.disconnect(function() {
             console.log('disconnected');
             process.exit(228);
         });
